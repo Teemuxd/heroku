754906a1ac49ec64af39dac7ab13a5a78d479009
Teemu I <teemu.itapelto@gmail.com>
2021-02-10 14:02:09 +0200

    asd

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask import render_template
+from sqlalchemy import create_engine
+import os
 
 app = Flask(__name__)
+
+DATABASE_URL = os.environ['DATABASE_URL']
+
+db = create_engine(DATABASE_URL)
 
 
 @app.route('/')
 def index():
-    return "Hello heroku! agen daadaadaaa 2"
+    results = db.execute('SELECT name FROM city')
+    return render_template('cities.html', cities=results)
 
 
 if __name__ == '__main__':
